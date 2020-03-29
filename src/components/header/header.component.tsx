a6ab6f1866589103ed05b6e96667674d4d5098f6
Artem Rebryk <artemrebrik8099@gmail.com>
2020-03-29 10:34:39 +0300

    public fix and create common classes

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,21 +1,18 @@
 import * as React from "react";
 
-import { IHeader } from "./header";
-import Link from "next/link";
-import { Nav } from "../nav";
-// import { IStore } from "../../app/core/store/IStore";
-// import { useSelector, useDispatch } from "react-redux";
-// import { ShoppingCartOutlined } from '@material-ui/icons';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
 import SearchIcon from '@material-ui/icons/Search';
-// import Badge from '@material-ui/core/Badge';
+
+import { IHeader } from "./header";
+import Link from "next/link";
+import { Nav } from "../nav";
 
 const Header: React.FunctionComponent<IHeader.IProps> = (): JSX.Element => {
   return (
-    <header>
-      <div className="header">
-        <div className="header-left">
+    <header className="header">
+      <div className="safe-area row header__content">
+        <div className="header-left row">
           <div className="logo">
             <Link href="/">
               <a><img src="/images/logo.svg" alt="logo" /></a>
@@ -23,7 +20,7 @@ const Header: React.FunctionComponent<IHeader.IProps> = (): JSX.Element => {
           </div>
           <Nav />
         </div>
-        <div className="header-right">
+        <div className="header-right row">
           <div className="tools">
             <SearchIcon />
             <BookmarkBorderIcon />
